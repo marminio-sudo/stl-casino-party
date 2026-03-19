8a93c03406c9c60a2992ba54d06064a44c65e675
marminio-sudo <marminio@gmail.com>
2026-03-19 17:08:11 -0500

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const SUPABASE_URL = 'https://rchcrpvwhnvcxgskcewb.supabase.co'
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3...'
+const url = 'https://rchcrpvwhnvcxgskcewb.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjaGNycHZ3aG52Y3hnc2tjZXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDgzMjQsImV4cCI6MjA4OTUyNDMyNH0.UFRFa2InKHXkblZTvrB0aIYen1zewDILEtlclDosC-Q'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(url, key)
