35b2c048845d39538abea4a49d6a788589b7cf12
marminio-sudo <marminio@gmail.com>
2026-03-19 17:02:31 -0500

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const SUPABASE_URL = 'https://rchcrpvwhnvcxgskcewb.supabase.co'
-export const SUPABASE_ANON_KEY = 'sb_publishable_q04W1lWXz9iIuGywUn5_Qw_QumySsmI'
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3...'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
